4596dd0247729f9e477a4dd3518c65d27b9054ee
wangkang <wkang2015@163.com>
2016-05-25 11:00:02 +0800

    Fix example.sh

--- src/bin/example.sh
+++ src/bin/example.sh
@@ -4,13 +4,13 @@ OutDir="output/"
 
 java \
  -jar     "lib/invar-1.0.1.jar" \
- -snippet "res/"
+ -snippet "res/" \
  -rule    "res/example/rule/" \
- -xsd     $OutDir."xsd/" \
- -cpp     $OutDir."cpp/" \
- -java    $OutDir."java/" \
- -csharp  $OutDir."csharp/" \
- -php     $OutDir."php/" \
- -flash   $OutDir."flash/" \
+ -xsd     ${OutDir}."xsd/" \
+ -cpp     ${OutDir}."cpp/" \
+ -java    ${OutDir}."java/" \
+ -csharp  ${OutDir}."csharp/" \
+ -php     ${OutDir}."php/" \
+ -flash   ${OutDir}."flash/" \
 
 exit 0
