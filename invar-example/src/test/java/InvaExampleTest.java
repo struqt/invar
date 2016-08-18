a9cb3b7a51635f0222b5b99b41cd71791930fa3e
wangkang <wkang2015@163.com>
2016-08-18 23:12:24 +0800

    Refactoring maven modules.

---
+++ invar-example/src/test/java/InvaExampleTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by wangkang on 8/18/16
+ */
+public class InvaExampleTest extends TestCase {
+
+    @Test
+    public void test() {
+    }
+
+
+}
