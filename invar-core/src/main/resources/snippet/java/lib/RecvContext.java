82b418451de1a64e4ad04e5afa8287b0d87fda14
wangkang <wkang2015@163.com>
2016-10-25 18:44:03 +0800

    Minor issue

--- invar-core/src/main/resources/snippet/java/lib/RecvContext.java
+++ invar-core/src/main/resources/snippet/java/lib/RecvContext.java
@@ -1,7 +1,17 @@
 package invar.lib;
 
+import java.net.SocketAddress;
+
 public interface RecvContext {
 
     String getSessionId();
+
+    String clientName();
+
+    long getCreateTime();
+
+    SocketAddress getLocalAddress();
+
+    SocketAddress getRemoteAddress();
 
 }
