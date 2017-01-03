62c005c114f400bade46c0cf93be67e07b8b1db7
wangkang <wkang2015@163.com>
2017-01-03 17:28:25 +0800

    Support custom session id.

--- invar-lib/src/main/java/invar/lib/RecvRequest.java
+++ invar-lib/src/main/java/invar/lib/RecvRequest.java
@@ -12,19 +12,32 @@ public abstract class RecvRequest<
     T extends InvarCodec.ProtocRequest,
     R extends InvarCodec.ProtocResponse> {
 
-    static Map<Class<?>, RecvRequest> map = new HashMap<Class<?>, RecvRequest>(256);
+    private static Map<Class<?>, RecvRequest> map = new HashMap<Class<?>, RecvRequest>(256);
 
     @SuppressWarnings("unchecked")
     static public <
         T extends InvarCodec.ProtocRequest,
         R extends InvarCodec.ProtocResponse,
-        C extends RecvContext> void recv(C ctx, T req, R resp) {
+        C extends RecvContext>
+    void recv(C ctx, T req, R resp) {
 
         if (map.containsKey(req.getClass())) {
-            RecvRequest recv = map.get(req.getClass());
+            RecvRequest<T, R> recv = map.get(req.getClass());
             recv.handle(req, resp, ctx);
         } else {
             resp.setProtocError(CodecError.ERR_PROTOC_NO_HANDLER);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static public <
+        T extends InvarCodec.ProtocRequest>
+    String getSessionId(T req) {
+        if (map.containsKey(req.getClass())) {
+            RecvRequest recv = map.get(req.getClass());
+            return recv.sessionId(req);
+        } else {
+            return null;
         }
     }
 
@@ -33,5 +46,9 @@ public abstract class RecvRequest<
     }
 
     public abstract void handle(T request, R response, RecvContext context);
+
+    protected String sessionId(T req) {
+        return null;
+    }
 
 }
